0f0d57e3d3688b21d6610eef2372a81bde462e60
komalrajbhar <komal.rajbhar@ratnamcollege.edu.in>
2023-12-06 02:45:26 +0530

    05-12-2023 Audit Table

--- app.py
+++ app.py
@@ -1,63 +1,74 @@
-from flask import Flask, render_template,request,redirect, flash,session,make_response,jsonify, session, url_for
+from flask import Flask, render_template, request, redirect, flash, session, make_response, jsonify, url_for
 from db_services import DBO
-from crypt_services import encrypt_sha256
-import random
-import string,os,json
-from pathlib import Path
-import flask
+from functools import wraps
+import os
 
 app = Flask(__name__)
 app.secret_key = 'file_upload_key'
 MYDIR = os.path.dirname(__file__)
-print("MYDIR",MYDIR)
+print("MYDIR", MYDIR)
 dbo = DBO()
+
+def login_required(view):
+    @wraps(view)
+    def wrapped_view(*args, **kwargs):
+        if 'session_var' not in session:
+            flash('Please login to access this page', 'warning')
+            return redirect(url_for('signin'))
+        return view(*args, **kwargs)
+    return wrapped_view
+
 
 
 @app.route('/')
 def signin():
-    return render_template('signin.html')                  
-                
+    return render_template('signin.html')
+
 @app.route("/render_login", methods=["GET", "POST"])
-def render_login(): 
-    form_data    = request.form
-    user_id      = form_data['username'].lower()
-    password     = form_data['password']
-    account      = dbo.get_cred(user_id)
-    enc_pass     = password
-    if(enc_pass == account["PASSWORD"]):        
-      session['session_var'] = {"user_id": user_id,
-                    "USER_NAME": account["FIRST_NAME"]+" "+account["LAST_NAME"],
-                    "USER_DEPARTMENT": account["USER_DEPARTMENT"],
-                    "USER_LEVEL": account["USER_LEVEL"]}
-      return make_response(render_template('dashboard.html',msg = True, err = False, warn = False, role=session['session_var']["USER_DEPARTMENT"],
-                                             level=session['session_var']["USER_LEVEL"]),200)
+def render_login():
+    form_data = request.form
+    user_id = form_data['username'].lower()
+    password = form_data['password']
+    account = dbo.get_cred(user_id)
+    enc_pass = password
+    if enc_pass == account["PASSWORD"]:
+        session['session_var'] = {"user_id": user_id,
+                                   "USER_NAME": account["FIRST_NAME"] + " " + account["LAST_NAME"],
+                                   "USER_DEPARTMENT": account["USER_DEPARTMENT"],
+                                   "USER_LEVEL": account["USER_LEVEL"]}
+        flash('Login Successful', 'success')
+        return redirect(url_for('dashboard'))
     else:
-      flash('Invalid Credentials')
-      return make_response(render_template("signin.html", msg = False, err = True, warn = False),403)   
-      
-      
+        flash('Invalid Credentials', 'danger')
+        return render_template("signin.html")
+
+
+    
 @app.route('/logout')
 def logout():
-    session.pop('user_id', None)
-    session.pop('selected_file', None)
+    session.pop('session_var', None)
     flash('Logout Successful')
-    return make_response(render_template("signin.html",msg = True,err = False, warn = False,message='Logout Successful'),200)  
+    return make_response(render_template("signin.html", msg=True, err=False, warn=False, message='Logout Successful'), 200)
 
-#################################### End Login logout ###################################### 
 
 @app.route('/dashboard')
+@login_required
 def dashboard():
     session_var = session.get('session_var', {})
-    return render_template('dashboard.html', session_var=session_var)
+    user_level = session['session_var'].get('USER_LEVEL', None)
 
+    if user_level == 'hod':
+        audit_data = dbo.fetch_audit_data_for_department(session_var.get("USER_DEPARTMENT", ""))
+    else:
+        audit_data = []
 
+    return render_template('dashboard.html', session_var=session_var, audit_data=audit_data)
 
-@app.route('/changecontrolform') 
+@app.route('/changecontrolform')
+@login_required
 def changecontrolform():
     session_var = session.get('session_var', {})
-    return render_template('changecontrolform.html', session_var=session_var)  
-    
-    
+    return render_template('changecontrolform.html', session_var=session_var)
 
 @app.route("/request_change_control", methods=['POST'])
 def request_change_control():
@@ -69,10 +80,16 @@ def request_change_control():
         existing_status = request.form.get('existingStatus')
         proposed_change = request.form.get('proposedChange')
         reason_justification = request.form.get('reasonJustification')
+
+        user_level = session['session_var']['USER_LEVEL']
+
         if area_of_change == 'Other':
             area_of_change = f'Other: {document_number}'
 
-        dbo.insert_change_control_data(area_of_change, initiator_department, description, existing_status, proposed_change, reason_justification)
+        dbo.insert_change_control_data(
+            area_of_change, initiator_department, description, existing_status, proposed_change, reason_justification,
+            level=user_level
+        )
 
         response = {"status": "success"}
         return redirect(url_for('changecontrolform'))
@@ -80,6 +97,19 @@ def request_change_control():
     except Exception as e:
         response = {"status": "error", "message": str(e)}
         return jsonify(response), 500
+
+@app.route('/update_form/<int:audit_id>')
+def update_form(audit_id):
+    record = dbo.get_audit_data_by_id(audit_id)
+    print("Record Data:", record)
+    return render_template('changecontrolform.html', record=record, is_update=True)
+
+@app.route("/update_form/<int:audit_id>", methods=['POST'])
+def update_form_submit(audit_id):
+    approval = request.form.get('approval')
+    remark = request.form.get('remark')
+
+    return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- db_services.py
+++ db_services.py
@@ -18,6 +18,7 @@ from crypt_services import encrypt_sha256
 import base64
 from base64 import b64encode
 from pathlib import Path
+from flask import session
 MYDIR        = os.path.dirname(__file__)
 
    
@@ -63,25 +64,105 @@ class DBO:
         except Exception as e:
             return e
         
+    def fetch_application_statuses(self):
+        try:
+            stmt = "SELECT DISTINCT Application_Status FROM ccndb.change_control_master"
+            conn = mysql.connector.connect(**config)
+            cursor = conn.cursor()
+            cursor.execute(stmt)
+            statuses = [row[0] for row in cursor]
+            return statuses
+        except Exception as e:
+            return []
+        
     def insert_change_control_data(self, area_of_change, initiator_department, description, existing_status, proposed_change, reason_justification, **kwargs):
         try:
             conn = mysql.connector.connect(**config)
             cursor = conn.cursor()
 
-            insert_query = """
+            insert_master_query = """
             INSERT INTO ccndb.change_control_master
             (Area_of_Change, Affects_Initiator_Department, Change_Description, Existing_Status, Proposed_Change, Change_Reason)
             VALUES (%s, %s, %s, %s, %s, %s)
             """
 
-            cursor.execute(insert_query, (area_of_change, initiator_department, description, existing_status, proposed_change, reason_justification))
+            cursor.execute(insert_master_query, (area_of_change, initiator_department, description, existing_status, proposed_change, reason_justification))
             conn.commit()
+
+            cursor.execute("SELECT LAST_INSERT_ID()")
+            last_inserted_id = cursor.fetchone()[0]
+
+            print(f"Last Inserted ID: {last_inserted_id}")
+
+            statuses = self.fetch_application_statuses()
+
+            for department in ['Production', 'QC', 'QA', 'Store', 'Engineering', 'HR', 'Admin']:
+                if kwargs.get('level') == 'hod':
+                    for status in statuses:
+                        insert_audit_query = """
+                        INSERT INTO ccndb.change_control_audit
+                        (Application_id, Status, Timestamp, Username, Department, Level, Remark)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s)
+                        """
+                        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                        username = session['session_var']['USER_NAME']
+
+                        try:
+                            cursor.execute(insert_audit_query, (last_inserted_id, status, timestamp, username, department, kwargs.get('level'), ''))
+                            conn.commit()
+
+                            print(f"Audit Record Inserted for Department: {department}, Status: {status}")
+                        except Exception as e:
+                            print(f"Error inserting audit data: {str(e)}")
+
 
             cursor.close()
             conn.close()
 
         except Exception as e:
             print(f"Error inserting data into the database: {str(e)}")
-            
+
+    def fetch_audit_data_for_department(self, department):
+        try:
+            stmt = """
+            SELECT a.Audit_id, m.Application_id, a.Status
+            FROM ccndb.change_control_audit a
+            JOIN ccndb.change_control_master m ON a.Application_id = m.Application_id
+            WHERE a.Department = %s
+            ORDER BY a.Timestamp DESC
+            """
+            conn = mysql.connector.connect(**config)
+            cursor = conn.cursor(dictionary=True)
+
+            cursor.execute(stmt, (department,))
+
+            audit_data = cursor.fetchall()
+
+            return audit_data
+        except Exception as e:
+            print(f"Error fetching audit data: {str(e)}")
+            return []
         
-        
+    def get_audit_data_by_id(self, audit_id):
+        try:
+            stmt = """
+            SELECT m.Application_id, m.Area_of_Change, m.Affects_Initiator_Department, m.Change_Description,
+                   m.Existing_Status, m.Proposed_Change, m.Change_Reason,
+                   a.Status as Audit_Status, a.Timestamp as Audit_Timestamp, a.Username, a.Remark
+            FROM ccndb.change_control_master m
+            JOIN ccndb.change_control_audit a ON m.Application_id = a.Application_id
+            WHERE a.Audit_id = %s
+            """
+            conn = mysql.connector.connect(**config)
+            cursor = conn.cursor(dictionary=True)
+
+            cursor.execute(stmt, (audit_id,))
+
+            audit_data = cursor.fetchone()
+
+            print(f"Audit Data for Audit ID {audit_id}: {audit_data}")
+
+            return audit_data
+        except Exception as e:
+            print(f"Error fetching audit data by ID: {str(e)}")
+            return None
